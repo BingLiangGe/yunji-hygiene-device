4510bc1bba6b7fd1f2e2a0e1ffbec51ca119b78e
peter-zhu <zzyoucandoit@163.com>
2025-07-22 10:36:41 +0800

    2025-07-11 项目：湿纸巾-设备 1 ping不去更新在线状态和在线时间

--- src/main/java/com/yunji/hygiene/service/DeviceCallService.java
+++ src/main/java/com/yunji/hygiene/service/DeviceCallService.java
@@ -34,11 +34,13 @@ public class DeviceCallService {
     public boolean ping(String imei, boolean reportStatus) {
         boolean ack = command(new EnterCommandDTO(TransEnum.PING.getCmd(), reportStatus ? 0 : -1, imei));
         //deviceService.updateStatus(imei, ack);
-        if (ack) {
-            deviceService.cabinetOnline(imei, true);
-        } else {
+        if (!ack) {
             deviceService.cabinetOffline(imei, true);
         }
+//        else {
+//
+//            deviceService.cabinetOnline(imei, true);
+//        }
         return ack;
     }
 
