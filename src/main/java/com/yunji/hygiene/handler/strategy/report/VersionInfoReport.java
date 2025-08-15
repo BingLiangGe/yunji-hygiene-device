557285bf1245d206b4ad150ad0ef51dd358bdbc2
peter-zhu <zzyoucandoit@163.com>
2025-08-15 17:40:09 +0800

    2025-08-15 项目：湿纸巾-设备-硬件服务器 1小时 1 加上比对版本功能 每次上报版本和当前最新版本比对

--- src/main/java/com/yunji/hygiene/handler/strategy/report/VersionInfoReport.java
+++ src/main/java/com/yunji/hygiene/handler/strategy/report/VersionInfoReport.java
@@ -18,9 +18,7 @@ import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
-
 /**
  * @author : peter-zhu
  * @date : 2025/2/19 16:14
@@ -29,7 +27,6 @@ import javax.annotation.Resource;
 @Service
 public class VersionInfoReport extends AbsTranReportMsg {
     private static final Logger log = LoggerFactory.getLogger(VersionInfoReport.class);
-
     @Resource
     private DeviceCallService deviceCallService;
 
@@ -43,13 +40,32 @@ public class VersionInfoReport extends AbsTranReportMsg {
         CommonResp resp = CommonResp.success(msg, AbsChannelReadHandler.getSerialNumber(ctx.channel()));
         UpgradeFilePO newest = deviceService.getFileByFileCodeLike(DeviceService.getVersionPrefix(vsp.getVersion()));
         if (newest != null) {
-            UpgradeCommandDTO upgradeCommandDTO = new UpgradeCommandDTO(TransStrategyEnum.DEVICE_GRADE.name(),
-                    -1, imei, newest.getId(), -1L);
-            deviceCallService.cacheFileUpgrade(upgradeCommandDTO);
+            //if (versionCompare(vsp.getVersion(), newest.getVersion())) {
+                log.info("VersionInfoReport versionCompare level up imei:{} reportVersion:{} currentVersion:{}"
+                        , imei, vsp.getVersion(), newest.getVersion());
+                UpgradeCommandDTO upgradeCommandDTO = new UpgradeCommandDTO(TransStrategyEnum.DEVICE_GRADE.name(),
+                        -1, imei, newest.getId(), -1L);
+                deviceCallService.cacheFileUpgrade(upgradeCommandDTO);
+//            } else
+//                log.info("VersionInfoReport versionCompare none imei:{} reportVersion:{} currentVersion:{}"
+//                        , imei, vsp.getVersion(), newest.getVersion());
         }
         return new TransReportDTO(true, true, resp);
     }
 
+    private static boolean versionCompare(String reportVersion, String fileVersion) {
+        if (reportVersion == null || fileVersion == null)
+            return false;
+        String currentV = fileVersion.substring(1);
+        int idx = reportVersion.lastIndexOf("-V");
+        String reportV = reportVersion.substring(idx + 2);
+        log.info("reportVersion:{} ====> currentVersion:{}", reportV, currentV);
+        return Integer.parseInt(currentV) > Integer.parseInt(reportV);
+    }
+//    public static void main(String[] args) {
+//        boolean rs = versionCompare("LC-SZJ-01-030-V25022819", "V25032817");
+//        System.out.println(rs);
+//    }
     @Override
     public ReportMsg getMsg(ByteBuf byteBuf) {
         return new GetVersionResp(byteBuf);
